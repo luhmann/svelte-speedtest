9189fb8cd705727d67dbf31ed151e5efb499c9d1
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2020-12-18 12:24:55 +0100

    chore: Make sever listen on all interfaces

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ void app.register(speedTestRoutes, { prefix: '/api/v1' });
 
 const start = async () => {
   try {
-    await app.listen(3000);
+    await app.listen(3000, '0.0.0.0');
     const address = app.server.address();
     const port = typeof address === 'string' ? address : address?.port;
     app.log.info(`server listening on ${String(port)}`);
